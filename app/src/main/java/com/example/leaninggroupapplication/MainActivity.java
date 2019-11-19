f20706784e41dca05da7d4aeefdf983b10ecc599
suminnn <57792461+suminnn@users.noreply.github.com>
2019-11-19 15:01:18 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/leaninggroupapplication/MainActivity.java
+++ app/src/main/java/com/example/leaninggroupapplication/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         setupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,CreatGroup.class);
+                Intent intent = new Intent(MainActivity.this,CreateGroup.class);
                 startActivity(intent);
             }
         });
