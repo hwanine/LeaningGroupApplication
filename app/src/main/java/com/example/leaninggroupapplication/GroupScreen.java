a723729a952dee4db606137d46065773a5d37727
suminnn <57792461+suminnn@users.noreply.github.com>
2019-11-26 03:43:20 +0900

    .

--- app/src/main/java/com/example/leaninggroupapplication/GroupScreen.java
+++ app/src/main/java/com/example/leaninggroupapplication/GroupScreen.java
@@ -4,13 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class GroupScreen extends AppCompatActivity {
+
+    Button gs_joinBtn;
+    Button sendFileBtn;
+    Button gs_commentBtn;
+    TextView gs_category;
+    TextView gs_title;
+    TextView gs_writer;
+    TextView gs_content;
+    TextView gs_numberOfUserMax;
+    TextView gs_numberOfUserNow;
+    TextView gs_date;
+    TextView gs_start_time;
+    TextView gs_end_time;
+    EditText gs_enterComments;
 
     //댓글 테스트
     //String userNameArray[]={"이수민","곽송이","오철원","이재환"};
